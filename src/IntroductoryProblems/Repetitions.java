a627a1779161fa8114dfdb822fa848372f6a6f1e
Prajwal Kuchewar <prajwalkuchewar3@gmail.com>
2025-01-08 22:08:09 +0530

    introductory problem #3

--- src/IntroductoryProblems/Repetitions.java
+++ src/IntroductoryProblems/Repetitions.java
@@ -1,4 +1,24 @@
 package IntroductoryProblems;
 
+import java.util.Scanner;
+
 public class Repetitions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int l = 0, r = 0;
+        int global = 0;
+        int len = 0;
+        while (r < s.length()) {
+            if (s.charAt(l) == s.charAt(r)) len += 1;
+            else {
+                l = r;
+                len = 1;
+            }
+            global = Math.max(len, global);
+            r++;
+        }
+        System.out.println(global);
+        sc.close();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            //int n = sc.nextInt();
+            System.out.println("write your code here..");
+        }
+        sc.close();
     }
 }
